67f8bc5fada5c66955dfe8e937b0d8906105d6cb
Gourav7777 <360inside360@gmail.com>
2023-03-25 16:45:00 +0530

    jkkl

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ const {connection} = require("./db")
  const {auth} = require("./Middleware/auth")
  const cors= require("cors")
 const app = express()
-app.use(cors())
+// app.use(cors())
+
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+ });
 app.use(express.json())
 app.use("/users",userroute)
 app.use(auth)
